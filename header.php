adfc778f1683ad70f42994b67c9a261f297add75
Zen1124 <kbulkiski@gmail.com>
2022-01-14 12:14:46 -0500

    Created Login Page and Button

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 <h4> <a href="explore.php">Explore</a> |
  <a href="create.php">Create</a> |
  <a href="support.php">Support</a> |
+ <a href="login.php">Login</a>
  <br>
 
 Find something new! </h4>
---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+    <meta name="google-signin-client_id" content="CLIENTID">
+    <title>Document</title>
+</head>
+<body>
+    <p>Sign in with Goodgle</p>
+</body>
+</html>
