8e158f39ab0618b7f3758a4d58f4bf9bf92e52dc
Zen1124 <kbulkiski@gmail.com>
2022-01-20 11:03:09 -0500

    Google stuff

--- login.php
+++ login.php
@@ -9,6 +9,19 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Sign in with Goodgle</p>
+    <p>Sign in with Google</p>
+    <div id="g_id_onload"
+         data-client_id="YOUR_GOOGLE_CLIENT_ID"
+         data-login_uri="https://your.domain/your_login_endpoint"
+         data-auto_prompt="false">
+      </div>
+      <div class="g_id_signin"
+         data-type="standard"
+         data-size="large"
+         data-theme="outline"
+         data-text="sign_in_with"
+         data-shape="rectangular"
+         data-logo_alignment="left">
+      </div>
 </body>
 </html>
