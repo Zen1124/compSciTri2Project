22d1746d51458e2c3b5c791275b095fdfc0adc30
Zen1124 <kbulkiski@gmail.com>
2022-01-26 14:16:29 -0500

    Added Button Code

--- login.php
+++ login.php
@@ -23,5 +23,22 @@
          data-shape="rectangular"
          data-logo_alignment="left">
       </div>
+      <script>
+        function handleCredentialResponse(response) {
+          console.log("Encoded JWT ID token: " + response.credential);
+        }
+        window.onload = function () {
+          google.accounts.id.initialize({
+            client_id: "YOUR_GOOGLE_CLIENT_ID",
+            callback: handleCredentialResponse
+          });
+          google.accounts.id.renderButton(
+            document.getElementById("buttonDiv"),
+            { theme: "outline", size: "large" }  // customization attributes
+          );
+          google.accounts.id.prompt(); // also display the One Tap dialog
+        }
+        </script>
+        <div id="buttonDiv"></div> 
 </body>
 </html>
