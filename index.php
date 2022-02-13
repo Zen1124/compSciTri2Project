a3056e3661639b26854777a73f4b057595824991
Zen1124 <kbulkiski@gmail.com>
2022-02-13 18:41:08 -0500

    Added Side Navbar

--- index.php
+++ index.php
@@ -13,138 +13,157 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RaiderRater</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<style>	
+		.sidenav {
+			width: 140px;
+			position: fixed;
+			z-index: 1;
+			top: 20px;
+			left: 10px;
+			background: #eee;
+			overflow-x: hidden;
+			padding: 8px 0;
+		}
+
+		.sidenav {
+			padding: 6px 8px 6px 16px;
+			text-decoration: none;
+			font-size: 15px;
+			color: #2196F3;
+			display: block;
+		}
+
+		.sidenav a:hover {
+			color: #064579;
+		}
+		
+		.container { 
+			margin-left: 150px; /* Same width as the sidebar + left position in px */
+			padding: 0px 10px;
+		} 
+		/* From w3 schools: https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp */
+	</style>
 </head>
 
 <body style="background-color:#afaeee;">
+<div class="container">
+	<?php
 
-<?php
+		#Connect to database server
+		//$dbc = mysqli_connect("localhost,"maxantonini","walterowl123","raiderrater") // XAMPP
+		$dbc = mysqli_connect("localhost","mantonini22webuser","reegmeems","raiderrater") // Regis
+			or die("Error: Cannot connect to database server");
 
-	#Connect to database server
-	//$dbc = mysqli_connect("localhost","maxantonini","walterowl123","raiderrater") // XAMPP
-	$dbc = mysqli_connect("localhost","mantonini22webuser","reegmeems","raiderrater") // Regis
-		or die("Error: Cannot connect to database server");
-if(isset($_SESSION["firstName"])) {
-	echo "Welcome " . $_SESSION["firstName"];
-	}
-else {
-	echo "Welcome " . "_____";
-	}
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Application Project (JAM Studios)</title>
-<br>
-<td><img src="images/logo.png" alt = "Raider Rater" width = "100" height = "100"></td>
-</head>
-<body>
-<table width="95%" border="0" cellspacing="0" cellpadding="0">
-<tr>
-	<td colspan="3"><center>
-	<div class="header"><h1> Welcome To The Raider Rater</h1></div>
-		<p><a href="userSession.php">Set your username</a></p>
-		<div style='text-align:left'> <h2> Search Bar </h2> </div>
+		if(isset($_SESSION["firstName"])) {
+			echo "Welcome " . $_SESSION["firstName"];
+			}
+		else {
+			echo "Welcome " . "_____";
+			}
+	?>
+	<br>
+	<td><img src="images/logo.png" alt = "Raider Rater" width = "100" height = "100"></td>
+	<table width="95%" border="0" cellspacing="0" cellpadding="0">
+			<tr> <!--HEADER-->
+				<td colspan="3"><center>
+				<div class="header"><h1> Welcome To The Raider Rater</h1></div>
+					<p><a href="userSession.php">Set your username</a></p>
+					<div style='text-align:left'> <h2> Search Bar </h2> </div>
 
-		<!-- Should have search bar and header links on the same line -->
-		<h4> <a href="explore.php">Explore</a> |
-		<a href="create.php">Create</a> |
-		<a href="support.php">Support</a> |
-		<br>
+					<!-- Should have search bar and header links on the same line -->
+					<h4> <a href="explore.php">Explore</a> |
+					<a href="create.php">Create</a> |
+					<a href="support.php">Support</a> |
+					<br>
 
-		Find something new! </h4>
-	</td>
-</tr>
-	</center>
-	</td>
-</tr>
-<tr>
-	<!-- Communities -->
-	<td class="column" width="15%" height="90%" style="text-align: center; margin: 10px; border: 3px solid red;">
-		<table class="column" width="100%" border="0" cellspacing="0" cellpadding="0">
-			<tr>
-				<td><a href="communities.php">Communities</a></td>
-			</tr>
-			<tr>
-				<td>
-				Sort By ↓ <br>
-				<b> New <br> <!-- Filters to sort posts on main page -->
-				Funny <br>
-				Popular <br>
-				Random <br> </b>
+					Find something new! </h4>
 				</td>
 			</tr>
-			<tr>
-				<td><a href="http://www.regis.org"></a></td>
-			</tr>
-		</table> 
-	</td>
-	<td colspan="3"><center>
-		<h2> Feed </h2>
-		<div style='text-align:left'>
-			<!-- Should have border around whole post, including image and comments -->
-			<p style="border:3px; border-style:solid; border-color:#FF0000; padding: 1em;">
-			<a href=" ">User Profile</a>
-			</p>
-			<img src="/raiderrater/images/mickey.png" alt = "Mickey Mouse" width = "200" height = "300">
-
-			<br>
-
-			<i>(Email Code embedded for comments and upvotes) </i> <br>
-			<a href=" ">Comments</a>
-			<br>
-
-			<!-- <img src="/raiderrater/images/daBaby.png" alt = "daBaby" width = "200" height = "300"> -->
-			<?php
-
-			#Where all the user posts are displayed
-				$sql = "SELECT * FROM rr_posts";
-				$rs = mysqli_query($dbc, $sql) or die(mysqli_error($dbc));
-				while ($row = mysqli_fetch_array($rs) ) {
-					echo '<div class="card" style="width: 100%;">';
-					echo '<h3 class="card-header">'. $row['postTitle']. '</h2>';
-					$locationID = $row['locationID'];
-					$rr_locations = mysqli_query($dbc, "SELECT * FROM rr_locations
-												WHERE locationID = $locationID ");
-					while ($loc = mysqli_fetch_array($rr_locations) ) { 
-						echo '<p>'. $loc['locationName']. ' | '. $loc['locationTypeID']. '</p>';
-						
-					}	
-					$picID = $row['pictureID'];
-					$rr_pics = mysqli_query($dbc, "SELECT * FROM rr_pics
-												WHERE pictureID = $picID ");
-					while ($pic = mysqli_fetch_array($rr_pics) ) { 
-						echo '<img src="'. 'images/'.$pic['fileName']. '"class="card-img-top" style="height: 10rem;"/>';
-					}
-					echo '	<div class="card-body">';
-					echo '		<p>'. $row['postContent']. '</p>';
-					echo $row['userID']. '<br>';
-					
-					echo $row['datePosted']. '<br>';
-					echo 'Upvotes: '. $row['upvoteCount']. ' | '. 'Downvotes: '. $row['downvoteCount']. '<br>';
-					echo '	</div>';
-					echo '</div>';
-				}
-			?>
-		</div>
 		</center>
-	</td>
-</tr>
-<tr>
-<td class="column" colspan="3"><center>
-	<div class="footer">
-		<center>
-		<br>
-		<br>
-		<a href="contact.php">Contact Us:</a>
-		<br>
-		<a href="http://www.regis.org">This project is sponsored by Regis High School</a>
+		</td>
+	</tr>
+		<div class="sidenav">
+			<a href="communities.php">Communities</a>
+			Sort By ↓ <br>
+			<a href=""><b> New <br></a> <!-- Filters to sort posts on main page -->
+			<a href="">Funny <br> </a>
+			<a href="">Popular <br></a>
+			<a href="">Random <br> </b></a>
+		</div>
+	
+		<td colspan="3"><center>
+			<h2> Feed </h2>
+			<div style='text-align:left'>
+				<!-- Should have border around whole post, including image and comments -->
+				<p style="border:3px; border-style:solid; border-color:#FF0000; padding: 1em;">
+				<a href=" ">User Profile</a>
+				</p>
+				<br>
+
+				<i>(Email Code embedded for comments and upvotes) </i> <br>
+				<a href=" ">Comments</a>
+				<br>
+
+				<!-- <img src="/raiderrater/images/daBaby.png" alt = "daBaby" width = "200" height = "300"> -->
+				<?php
+
+				#Where all the user posts are displayed
+					$sql = "SELECT * FROM rr_posts";
+					$rs = mysqli_query($dbc, $sql) or die(mysqli_error($dbc));
+					while ($row = mysqli_fetch_array($rs) ) {
+						echo '<div class="card" style="width: 100%;">';
+						echo '<h3 class="card-header">'. $row['postTitle']. '</h2>';
+						//Location & Type
+						$locationID = $row['locationID'];
+						$rr_locations = mysqli_query($dbc, "SELECT * FROM rr_locations
+													WHERE locationID = $locationID ");
+						while ($loc = mysqli_fetch_array($rr_locations) ) { 
+							echo '<h6 class="location" style="padding-left: 10px;">'. $loc['locationName']. ' | '. $loc['locationTypeID']. '</h6>';
+							
+						}	
+						//Picture
+						$picID = $row['pictureID'];
+						$rr_pics = mysqli_query($dbc, "SELECT * FROM rr_pics
+													WHERE pictureID = $picID ");
+						while ($pic = mysqli_fetch_array($rr_pics) ) { 
+							echo '<img src="'. 'images/'.$pic['fileName']. '"class="card-img-top" style="height: 10rem;"/>';
+						}
+						echo '	<div class="card-body">';
+						echo '		<p>'. $row['postContent']. '</p>';
+						echo '		<div style="font-size: 11px;">';
+						//Username
+						$userID = $row['userID'];
+						$rr_users = mysqli_query($dbc, "SELECT * FROM rr_users
+													WHERE userID = $userID ");
+						while ($usr = mysqli_fetch_array($rr_users) ) { 
+							echo '<p id="username">'. $usr['userName']. '</p>';
+						}
+						echo '			<p id="datePosted">'. $row['datePosted']. '<br> </p>';
+						echo '			<span style="display: inline;"> <div class="upvote" style="display: inline;">Upvotes: </div>'. $row['upvoteCount']. ' | '. '<div class="downvote" style="display: inline;">Downvotes: </div>'. $row['downvoteCount'].  '<br></span>';
+						echo '		</div>';
+						echo '	</div>';
+						echo '</div>';
+					}
+				?>
+			</div>
+			</center>
+		</td>
+	</tr>
+	<tr>
+	<td class="column" colspan="3"><center>
+		<div class="footer">
+			<center>
+			<br>
+			<br>
+			<a href="contact.php">Contact Us:</a>
+			<br>
+			<a href="http://www.regis.org">This project is sponsored by Regis High School</a>
+		</center>
+		</div>
 	</center>
-	</div>
-</center>
-</td>
-</tr>
-</table>
+	</td>
+	</tr>
+	</table>
 </body>
 </html>
 
@@ -170,6 +189,6 @@ $sql = "SELECT * FROM rr_users";
 	}
 
 ?>
-
+</div>
 </body>
 </html>
